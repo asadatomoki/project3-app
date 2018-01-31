c2d058391aa3cbf70a71e679a0bc1f509fe854af
朝田友紀 <kd1253285@st.kobedenshi.ac.jp>
2018-01-31 05:29:41 +0000

    01/31 第3回課題提出

--- myapp/app/assets/javascripts/application.js
+++ myapp/app/assets/javascripts/application.js
@@ -15,140 +15,17 @@
 //= require turbolinks
 //= require_tree .
 
+/* global $ */
 
-
-
-
-
-
-<div id="container">
-	<h1 class="indent"></h1>
-<jpg src="gurando.jpg" alt="gurando" height="400" width=400 />
-
- 
- 
- 
-
-
-
-
-</script>
-</head>
-<body>
-  <div id="img1" style="left:500px; top:50px;position:absolute;">
-
-       <img src="file:///C:/Users/%E6%9C%9D%E7%94%B0%E5%8F%8B%E7%B4%80/Desktop/bandicam%202018-01-31%2009-24-07-860.jpg" width="820" height="520"></div>
-</body>
-
-
-
-
-
-
-
-
-
-</div>
-
- <div>
- <button type="button">メニュー</button>
-  
-
-
- 
- 
-  <table style="text-align: left; border:0; padding:0.2em; margin-bottom:0.2em; background-color:green; color:white; text-align:center">
-<div class="scroll_wrapper">
-  <table>
-    <thead>
-      <tr>
-        <th>&nbsp;</th>
-        <th>1</th>
-        <th>2</th>
-        <th>3</th>
-        <th>4</th>
-        <th>5</th>
-        <th>6</th>
-        <th>7</th>
-        <th>8</th>
-        <th>9</th>
-        <td class="total-1">計</td>
-        <td class="total-2">H</td>
-        <td class="total-2">E</td>
-      </tr>
-    </thead>
-    <tbody>
-      <tr class="top">
-        <th><span class="flag_h">donnn</span></th>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>1</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td class="total-1">1</td>
-        <td class="total-2">9</td>
-        <td class="total-2">1</td>
-      </tr>
-      <tr class="bottom">
-        <th><span class="flag_c">kokoko</span></th>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>1</td>
-        <td>0</td>
-        <td class="total-1">1</td>
-        <td class="total-2">4</td>
-        <td class="total-2">0</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
- <table style="text-align: left; border:0; padding:0.2em; margin-bottom:0.2em; background-color:green; color:white; text-align:center">
-<div class="scroll_wrapper">
-  <table>
-    <thead>
-     
-   
-
-    
-
-
-
-
-
-
-
-	
-
-
-   <div class="button" tabindex="0">
-<a class="button" tabindex="0">
-<a href=><button type="button">保存</button>
-
-　
-
-<div class="noprint">
-    <a href="#" onclick="window.print(); return false;">印刷する</a>
-</div>
-
-
-		
-	</div>
-</div>
-<div class="contents-box-head">
-
-
-
-
-
-
-
-</body></html>
+$(function(){
+  $("table#scoretable td").hover(function() {
+    //縦
+    var row = $(this).closest('tr').index();
+    //横
+    var col = this.cellIndex + 1;
+    console.log('Row: ' + row + ', Column: ' + col);
+    $(this).css("background-color","#f99");
+  }, function() {
+    $(this).css("background-color","#fff");
+  });
+});
